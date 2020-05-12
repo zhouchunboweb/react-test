374bb2b8f17257ca333840a2866885a8362419d3
zhouchunboweb <153566945@qq.com>
2020-05-12 10:29:49 +0800

    first

---
+++ src/Page2.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Page2 extends React.Component {
+  render () {
+    let mystyle={
+          width:'200px',
+          height:'80px',
+          backgroundColor:'yellow',
+          fontSize:'24px',
+          textAlign:'center'
+        }
+    return(
+      <div>
+        <div style={mystyle}>This is Page2!</div>
+      </div>
+    )
+  }
+}
+
+export default Page2
