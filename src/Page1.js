42c0dffa8e09ea55c4ec88da7515207f6aa52e00
zhouchunboweb <153566945@qq.com>
2020-05-12 11:03:39 +0800

    cs

---
+++ src/Page1.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Page1 extends React.Component {
+  render () {
+    return(
+      <div>
+        <div
+          style={{
+            fontSize:'26px',
+            width:'200px',
+            height:'80px',
+            backgroundColor:'red'
+          }} >This is Page1!</div>
+      </div>
+    )
+  }
+}
+
+export default Page1
---
+++ src/Page3.js
@@ -0,0 +1,14 @@
+import React from 'react'
+require('./Page3.css')
+
+class Page3 extends React.Component {
+  render () {
+    return(
+      <div>
+        <div className='mystyle'>This is Page3!</div>
+      </div>
+    )
+  }
+}
+
+export default Page3
